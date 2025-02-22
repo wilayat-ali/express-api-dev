69e62075e1a01f95b750d5d5876d62efbd1f93d7
wilayat8070 <wilayat@360i.agency>
2025-02-23 00:13:33 +0500

    change port

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.post("/api/data", (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
